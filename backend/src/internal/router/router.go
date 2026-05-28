c3b1a72b3d436a9563aeb5cdc6a519a71166f154
ylly-nn <ylly_nn@mail.ru>
2026-05-28 14:12:38 +0400

    feat: добавленна регистрация

---
+++ backend/src/internal/router/router.go
@@ -0,0 +1,38 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	chimiddleware "github.com/go-chi/chi/v5/middleware"
+	httpSwagger "github.com/swaggo/http-swagger"
+
+	_ "src/docs"
+	"src/internal/auth"
+	"src/internal/middleware"
+)
+
+func New(authMiddleware *middleware.AuthMiddleware, authHandler *auth.Handler) http.Handler {
+	r := chi.NewRouter()
+
+	// Глобальные middleware для всех запросов
+	r.Use(chimiddleware.Logger)    // логирование запросов
+	r.Use(chimiddleware.Recoverer) // восстановление после паник
+
+	// Swagger UI — доступен по /swagger/index.html
+	r.Get("/swagger/*", httpSwagger.Handler(
+		httpSwagger.URL("/swagger/doc.json"), // обязательно!
+	))
+
+	r.Route("/auth", func(r chi.Router) {
+		r.Post("/register", authHandler.Register)
+		r.Post("/verify", authHandler.VerifyCode)
+		r.Post("/login", authHandler.Login)
+		r.Post("/refresh", authHandler.Refresh)
+		r.Post("/logout", authHandler.Logout)
+		r.Post("/forgot-password", authHandler.ForgotPassword)
+		r.Post("/verify-reset-code", authHandler.VerifyResetCode)
+		r.Post("/set-password", authHandler.SetPassword)
+	})
+	return r
+}
